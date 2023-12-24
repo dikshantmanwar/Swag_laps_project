4a3e45e601ed542c6d7181d94581e4349bfb7b13
Dikshant <dikshantmanwar97@gmail.com>
2023-12-24 19:50:56 +0530

    update_login test

--- testCases/test_Login.py
+++ testCases/test_Login.py
@@ -1,42 +1,53 @@
+from Utilities.readproperties import ReadValue
+from PageObject.LoginPage import LoginPage
+from Utilities.Logger import LogGen
 
-from Utilitis.readproperties import ReadValue
-from PageObject.LoginPage import Login
-from Utilitis.Logger import LogGen
 
-class Test_Url_login():
+class TestUrlLogin:
+    """
+    Test class for login functionality.
+    """
 
-    url=ReadValue.getUrl()
-    username=ReadValue.getusername()
-    password=ReadValue.getPassword()
-    log=LogGen.loggen()
-    def test_login(self,setup):
+    URL = ReadValue.getUrl()
+    USERNAME = ReadValue.getusername()
+    PASSWORD = ReadValue.getPassword()
+    LOG = LogGen.loggen()
 
-        self.driver=setup
-        self.driver.get(self.url)
+    def test_login(self, setup):
+        """
+        Test method for the login functionality.
 
-        self.lp=Login(self.driver)
+        :param setup: The test setup.
+        :return: None
+        """
 
-        self.lp.get_username(self.username)
-        self.log.info("Entering username----->"+ self.username)
-        self.lp.get_password(self.password)
-        self.log.info("Entering password------>"+ self.password)
+        self.driver = setup
+        self.driver.get(self.URL)
+
+        self.lp = LoginPage(self.driver)
+
+        self.lp.get_username(self.USERNAME)
+        self.LOG.info("Entering username ----->" + self.USERNAME)
+        self.lp.get_password(self.PASSWORD)
+        self.LOG.info("Entering password ------> " + self.PASSWORD)
         self.lp.click_on_login()
 
-        self.log.info("click on log in button")
-        if self.lp.login_status()==True:
-            self.log.info('login test is pass')
+        self.LOG.info("Click on login button")
+        if self.lp.login_status():
+            self.LOG.info('Login test is pass')
             self.driver.save_screenshot(
-                'C:\\Users\\Admin\\PycharmProjects\\Swag_Labs_Project\\Screenshot\\login_succesfull.png')
+                'C:\\Users\\Admin\\PycharmProjects\\Swag_Labs_Project\\Screenshot\\login_successful.png')
             self.lp.click_on_menu()
             self.lp.click_on_logout()
-            self.log.info("click on logout button")
-            self.driver.save_screenshot('C:\\Users\\Admin\\PycharmProjects\\Swag_Labs_Project\\Screenshot'
-                                    '\\logout_succesfull.png')
+            self.LOG.info("Click on logout button")
+            self.driver.save_screenshot(
+                'C:\\Users\\Admin\\PycharmProjects\\Swag_Labs_Project\\Screenshot\\logout_successful.png')
             assert True
         else:
-            self.log.info("test_login is Failed")
+            self.LOG.info("Test login is Failed")
             self.driver.save_screenshot(
                 "C:\\Users\\Admin\\PycharmProjects\\Swag_Labs_Project\\Screenshot\\login_fail.png")
             assert False
+
         self.driver.close()
-        self.log.info("test_login is Completed")
+        self.LOG.info("Test login is Completed")
