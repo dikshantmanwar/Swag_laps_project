b4bdbc4d3eb63e2a96a87d48b61afb8f8c66ec15
Dikshant <dikshantmanwar97@gmail.com>
2023-12-30 11:03:37 +0530

    commite in master banch

--- Run.bat
+++ Run.bat
@@ -5,4 +5,3 @@ pytest -rA  --browser edge testCases/test_Login.py
 pytest -rA  --browser chrome testCases/test_Login.py
 pytest -rA -n=4  --browser chrome testCases/test_login_param.py
 pytest -rA -n=4  --browser chrome --html=Reports/report_param.html  testCases/test_login_param.py
-
